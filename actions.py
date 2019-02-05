3fd50447a11a5ce3db513456a21a6c7cd4b93014
anassu1400 <anassu1400@gmail.com>
2019-02-05 09:09:23 +0300

    project-3

--- actions.py
+++ actions.py
@@ -2,9 +2,9 @@
 from data import  population, clubs
 from components import Club, Person
 
-my_name = ""
-my_age = -1
-my_bio = ""
+my_name = "Anas"
+my_age = 20
+my_bio = "Person of a personal personage, known only on a person-to-person basis."
 myself = Person(my_name, my_bio, my_age)
 
 def introduction():
@@ -12,25 +12,90 @@ def introduction():
 
 def options():
     # your code goes here!
+    print("-------------")
+    option = input("1) Crete a new club.\nor:\n2) Browse and join a club.\nor:\n3) View existing clubs.\nor:\n4) Display members of a club.\nor\n-1) Close application.\n")
+    return option
+
+
     
 
 def create_club():
     # your code goes here!
+    club_name = input("What is the name of your awesome new club: ")
+    club_desc = input("What is your club about?\n")
+    club = Club(club_name, club_desc)
+    club.members.append(myself)
+    club.assign_president(myself)
+    m = []
+    print("Enter the numbers of people you would like to recruit to your club(-1 to stop): \n")
+    n = 1
+    for i in population:
+    	print("[%d] %s" % (n, i.name))
+    	n = n +1
+    mem = input()
+    memb = int(mem)
+    while mem != -1:
+    	memb = memb -1
+    	club.members.append(population[memb])
+    	mem = input("Who else: ")
+    	memb = int(mem)
+    	if memb == -1:
+    		break
+    clubs.append(club)
+    print("Here's your new club:\n %s\n%s\n" % (club.name, club.description))
+    club.print_member_list()
+
     
 
 def view_clubs():
     # your code goes here!
     
+    for club in clubs:
+    	m = 0
+    	for member in club.members:
+    		m = m + 1 
+    	print("\tNAME: %s.\n\tDISCRIPTION: %s.\n\tMEMBERS: %d.\n" % (club.name, club.description, int(m)))
+
+    
 
 def view_club_members():
     # your code goes here!
+    view_clubs()
+    club = input("Enter the name of the club whose members you'd like to view: ")
+    for c in clubs:
+    	if club == c.name:
+    		c.print_member_list()
+
+
     
 
 def join_clubs():
     # your code goes here!
+    view_clubs()
+    club = input("Enter the name of the club you'd like to join: ")
+    for c in clubs:
+    	if club == c.name:
+    		c.recruit_member(myself)
+    		print("%s just joing the %s\n------------------" % (my_name, c.name))
     
 
 def application():
     introduction()
     # your code goes here!
+    option = 0
+    while option != -1:
+    	option = int(options())
+    	if option == 1:
+            create_club()
+    	elif option == 2:
+            join_clubs()
+    	elif option == 3:
+            view_clubs()
+    	elif option == 4:
+	        view_club_members()
+    	else:
+	    	break
+    else:
+        exit()
+
     
--- components.py
+++ components.py
@@ -2,21 +2,47 @@
 class Person():
     def __init__(self, name, bio, age):
         # your code goes here!
+        self.name = name
+        self.bio = bio
+        self.age = age
+        
 
 
 class Club():
     def __init__(self, name, description):
         # your code goes here!
+        self.name = name
+        self.description = description
+        self.president = ""
+        self.members = []
 
 
     def assign_president(self, person):
         # your code goes here!
+        for member in self.members:
+            if member == person:
+                self.president = member
+
 
 
     def recruit_member(self, person):
         # your code goes here!
+        self.members.append(person)
 
 
     def print_member_list(self):
         # your code goes here!
+        print("Members: ")
+        age = 0.0
+        m = 0.0
+        for member in self.members:
+            if self.president == member:
+                print("- %s (%s years old, %s) - %s." % (member.name, str(member.age), "President", member.bio))
+                age = age + int(member.age)
+            else:
+                print("- %s (%s years old) - %s." % (member.name, str(member.age), member.bio))
+                age = age + int(member.age)
+            m = m + 1
+        average_age = age / m
+        print("Average ag in this club: %.2fyr\n---------------" % (average_age))
 
